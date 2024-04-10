9871f482e8de2a72eb4c0e6c21ead83ce0aec419
jqkim6 <jqkim6@gmail.com>
2024-04-10 18:35:51 -0400

    Update Finish Button in SecondAddActivity

--- test/MyApplication2/app/src/main/java/com/example/myapplication/SecondAddActivity.java
+++ test/MyApplication2/app/src/main/java/com/example/myapplication/SecondAddActivity.java
@@ -22,7 +22,7 @@ public class SecondAddActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondAddActivity.this, FirstAddActivity.class);
+                Intent intent = new Intent(SecondAddActivity.this, FirstActivity.class);
                 startActivity(intent);
             }
         });
