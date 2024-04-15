1bcbf26ab2c0fc3abde5e87eaf56c22fefd4c07b
jqkim6 <jqkim6@gmail.com>
2024-04-15 14:30:38 -0400

    add date picker

--- test/MyApplication2/app/src/main/java/com/example/myapplication/FirstAddActivity.java
+++ test/MyApplication2/app/src/main/java/com/example/myapplication/FirstAddActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 import androidx.activity.EdgeToEdge;
@@ -13,10 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class FirstAddActivity extends AppCompatActivity {
 
@@ -32,7 +37,7 @@ public class FirstAddActivity extends AppCompatActivity {
                 ArrayList<String> taskitem=new ArrayList<>();
                 String title=((TextInputEditText)findViewById(R.id.textInputTitle)).getText().toString();
                 String category=((TextInputEditText)findViewById(R.id.textInputCategory)).getText().toString();
-                String deadline=((TextInputEditText)findViewById(R.id.textInputDeadline)).getText().toString();
+                String deadline=((EditText)findViewById(R.id.editTextDeadline)).getText().toString();
                 String workload=((TextInputEditText)findViewById(R.id.textInputWorkload)).getText().toString();
                 taskitem.add(title);
                 taskitem.add(category);
@@ -48,5 +53,19 @@ public class FirstAddActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void showDatePickerDialog(View view) {
+        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select deadline");
+        final MaterialDatePicker<Long> datePicker = builder.build();
+
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            String dateString = formatter.format(new Date(selection));
+            ((EditText) findViewById(R.id.editTextDeadline)).setText(dateString);
+        });
+
+        datePicker.show(getSupportFragmentManager(), "datePicker");
     }
 }
